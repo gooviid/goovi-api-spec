4cdf3e23402fd76fc8097b11d4d41580d4f47287
Handi Deyana <h.deyana@gmail.com>
2022-06-13 17:26:06 +0700

    Init

---
+++ generated-typescript/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AudioInfo';
+export * from './BillingDetail';
+export * from './CreateTranscode';
+export * from './ImageInfo';
+export * from './MediaInfo';
+export * from './SourceTranscode';
+export * from './TranscodeError';
+export * from './TranscodeMeta';
+export * from './TranscodeQueue';
+export * from './VideoInfo';
